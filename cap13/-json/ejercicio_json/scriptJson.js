8b5fcb2dd8e26180dc2c286512cd1c7919c2b63e
Joselin Zarate <zaratejoselin594@gmail.com>
2023-07-08 18:53:52 -0300

    js

--- cap13/-json/ejercicio_json/scriptJson.js
+++ cap13/-json/ejercicio_json/scriptJson.js
@@ -1,6 +1,7 @@
 /*Proyecto: Conversion de datos en formato JSON a tablas
-Objetivo: El objetivo de este proyecto es tomar un archivo de datos en formato JSON y convertirlo en una tabla HTML dinámica utilizando JavaScript. Esto permitirá mostrar los datos de una manera más visual y fácil de entender para los usuarios.*/ 
+Objetivo: El objetivo de este proyecto es tomar un archivo de datos en formato JSON y convertirlo en una tabla HTML dinámica utilizando JavaScript. Esto permitirá mostrar los datos de una manera más visual y fácil de entender para los usuarios.*/
 
+//json serializado
 let frutasJson = `{
    "frutas": [
     {
@@ -30,23 +31,29 @@ let frutasJson = `{
   ]
 }`;
 
+//lo deserializamos para poder usar los datos del json
 let frutasJsonDes = JSON.parse(frutasJson);
 
+//realizamos el evento de que cuando cargue la pagina se realicen las unciones
 window.addEventListener("load", () => {
+  //colocamos el nombre de las columnas
   crearColumnas("Nombre")
   crearColumnas("Color")
   crearColumnas("Sabor")
   crearColumnas("Precio")
+  //colocamos los datos
   crearLineas(frutasJsonDes.frutas[0].nombre, frutasJsonDes.frutas[0].color, frutasJsonDes.frutas[0].sabor, frutasJsonDes.frutas[0].precio)
   crearLineas(frutasJsonDes.frutas[1].nombre, frutasJsonDes.frutas[1].color, frutasJsonDes.frutas[1].sabor, frutasJsonDes.frutas[1].precio)
   crearLineas(frutasJsonDes.frutas[2].nombre, frutasJsonDes.frutas[2].color, frutasJsonDes.frutas[2].sabor, frutasJsonDes.frutas[2].precio)
   crearLineas(frutasJsonDes.frutas[3].nombre, frutasJsonDes.frutas[3].color, frutasJsonDes.frutas[3].sabor, frutasJsonDes.frutas[3].precio)
 })
+//creamos una funcion que cree las columnas necesarias
 const crearColumnas = (nombre) => {
   let columna = document.createElement("th");
   document.getElementById("thead").appendChild(columna)
   columna.textContent = nombre
 }
+//creamos una funcion que cree las lineas con los datos e cada columna de la tabla
 const crearLineas = (fNombre, fColor, fSabor, fPrecio) => {
   let linea = document.createElement("tr");
   document.getElementById("tbody").appendChild(linea)
