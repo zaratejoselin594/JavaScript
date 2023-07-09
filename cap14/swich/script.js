ec39519db9b14f69b1f8eaff69e36677d5992a7a
Joselin Zarate <zaratejoselin594@gmail.com>
2023-07-09 18:55:42 -0300

    js

---
+++ cap14/swich/script.js
@@ -0,0 +1,19 @@
+let fruta = "banana";
+
+switch (fruta) {
+  case "manzana":
+    console.log("Es manzana");
+    break;
+  case "pera":
+    console.log("Es pera");
+    break;
+  case "banana":
+    console.log("Es banana");
+    break;
+  case "durazno":
+    console.log("Es durazno");
+    break;
+  default:
+    console.log("no es ninguna");
+    break;
+}
